61ecd7e9dc42e0342e4460a49617425e2160651b
blindtag <phylace0@gmail.com>
2022-01-18 19:32:36 -0800

    'scaffold'

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Login from '../components/Login'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+        <Login/>
+
+    </div>
+  )
+}
